af87fb7c3ff87f5dbc231822420130f069d8d74f
Ivanitsky Alexey <to.developer.aleksey@gmail.com>
2023-02-28 22:25:04 +0300

    Updated tracing

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:${springVersion}")
     implementation("org.springframework.boot:spring-boot-starter-validation:${springVersion}")
     implementation("org.springframework.boot:spring-boot-starter-web:${springVersion}")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:${springVersion}")
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.414")
     implementation("org.apache.commons:commons-io:1.3.2")
     implementation("org.liquibase:liquibase-core:4.18.0")
@@ -39,11 +40,10 @@ dependencies {
 
     // add for generate traceId and spanId
     implementation("io.micrometer:micrometer-tracing-bridge-brave:1.0.1")
-    implementation("io.micrometer:micrometer-tracing:1.0.1")
+    implementation("io.micrometer:micrometer-observation:1.10.4")
     implementation("io.zipkin.reporter2:zipkin-reporter-brave:2.16.3")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:${springVersion}")
-    testImplementation("io.projectreactor:reactor-test")
     testImplementation("com.h2database:h2")
 }
 
